2d22a6fc3f121f504e1f19af8c33c4d81673ad24
WalaGithub <fabriziodedios@gmail.com>
2026-03-02 13:54:36 -0600

    t

--- src/Checker.java
+++ src/Checker.java
@@ -6,34 +6,6 @@ public class Checker extends Hitbox {
         super(ex, wy, wd, ht);
         color=c;
     }
-  /*
-    /*public void bouncePaddle(Paddle o){
-        double midX=getX()+ getW()/2;
-        double midXo=o.getX()+o.getW()/2;
-        if(intersects(o)){
-            ySpeed=-ySpeed;
-            double xsp=midX-midXo;
-            int fob;
-            if(midX>=midXo){
-                fob=1;
-            }
-            else{
-                fob=-1;
-            }
-//            if(xsp>=-10 && xsp<=10){
-//                xSpeed=0;
-//            }
-//            else if(xsp>=-70 && xsp<=70){
-//                xSpeed=fob;
-//            }
-//            else if(xsp>=-150 && xsp<=150){
-//                xSpeed=2*fob;
-//            }
-//            else{
-//                xSpeed=3 *fob;
-//            }
-        }
-    }*/
  public void paint(Graphics window) {
 //
 }
--- src/Hitbox.java
+++ src/Hitbox.java
@@ -20,40 +20,7 @@ public class Hitbox extends Canvas {
 		w=wd;
 		h=ht;
 	}
-	// All Bricks will have all of these set and get methods
-	//these methods are complete
-	public int getX( ){
-		return x;
-	}
-	public void setX( int ex ){
-		x = ex;
-	}
-	public int getY( ){
-		return y;
-	}
-	public void setY( int wy ){
-		y = wy;
-	}
-	public int getW(){
-		return w;
-	}
-	public int getH(){
-		return h;
-	}
 
-	//code to see if this Brick intersects with the other Brick goes here
-	public boolean intersects( Hitbox other ) {
-		/* Easy Way:
-		 * 		Copy and Paste the following URL in Google Chrome mm
-		 *		https://docs.oracle.com/javase/8/docs/api/java/awt/Rectangle.html
-		 *		make new Rectangle, using this Brick's x,y,w,h
-		 *		make new Rectangle, using the other Bricks's x,y,w,h
-		 *		return if the two Rectangles intersect, use the Rectangle's intersects method.
-		 */
-		Rectangle a=new Rectangle(x,y,w,h);
-		Rectangle b=new Rectangle(other.getX(),other.getY(),other.getW(),other.getH());
-		return a.intersects(b);
-	}
 	//code to draw the Brick will go here
 	public void paint( Graphics window ) {
 		//Draw the Brick with Graphics methods
@@ -64,7 +31,7 @@ public class Hitbox extends Canvas {
 //		window.setColor(Color.WHITE);
 //		window.drawRect(getX(), getY(), getW(), getH());
 
-        Image baguette=Toolkit.getDefaultToolkit().getImage("mushroom_1.png");
+        Image baguette=Toolkit.getDefaultToolkit().getImage("pad.png");
         window.drawImage(baguette, x+(w/2)/30*30, y-(h/2)/30*30, w, h, this);
 	}
 }

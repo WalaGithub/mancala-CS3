267df76922a2704950b032efa3384ddc7efc89f9
armaanpkamdar-arch <armaanpkamdat@gmail.com>
2026-03-04 13:47:53 -0600

    I hate fabrizio

--- src/Player.java
+++ src/Player.java
@@ -1,17 +1,10 @@
-import java.util.Scanner;
-public class Player {
-    final String name;
+ipublic class Player {
     final char c;
-    final Scanner scanner=new Scanner(System.in);
     public Player(char color){
         c=color;
-        System.out.println("Player "+c+" please type your name");
-        name=scanner.nextLine();
     }
     public void turnStart(Dice d1, Dice d2){
         System.out.println("Please roll dice and hit enter when complete");
-        d1.canRoll=true;
-        System.out.println(scanner.nextLine());
         System.out.println("Thank you");
     }
 }
